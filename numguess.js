d0d0c2ac6eb4ebed211cfb981a3f9ea60f895413
KrisZeynev <kriszeynev@yahoo.com>
2023-01-21 12:39:26 +0200

    Update numguess.js
    Updating the result text

--- numguess.js
+++ numguess.js
@@ -9,13 +9,13 @@ document.getElementById("submitBtn").onclick = function(){
     guesess += 1;
 
     if (guess > 100){
-        res.textContent = "You're over the range";
+        res.textContent = "You're over the range!";
     } else if (guess < 0){
-        res.textContent = "You're under the range";
+        res.textContent = "You're under the range!";
     } else if(guess == ""){
         res.textContent = "Please enter number bewteen 0 - 100!";  
     } else if(guess == answer) {
-        res.textContent = `The answer is ${answer}. It took you ${guesess} guesess`;
+        res.textContent = `The answer is ${answer}. It took you ${guesess} guesess!`;
     } else if (guess < answer) {
         res.textContent = "Too small!"
         document.getElementById("guessField").value = ""; 
